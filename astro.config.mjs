004fbe853fd4ee467c941533b5f36d18fc47e468
maksimkadocnikov <maxim.kadocnikov@scorum.com>
2023-09-03 22:09:15 +0700

    upload functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 import {defineConfig} from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
+import icon from "astro-icon";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), solidJs()],
+    integrations: [tailwind(), solidJs(), icon({iconDir: 'src/icons'})],
     output: 'server',
-    adapter: cloudflare()
+    adapter: cloudflare({mode: 'directory', functionPerRoute: true}),
+    image: {
+        service: {
+            entrypoint: 'astro/assets/services/noop'
+        }
+    }
+
 });
